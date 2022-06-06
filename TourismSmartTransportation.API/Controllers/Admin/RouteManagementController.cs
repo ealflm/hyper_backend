c84ea7bee55f355682540809dc235e687217b1f2
danhnlc <danhnlc252k@gmail.com>
2022-06-06 12:55:00 +0700

    create route api

--- TourismSmartTransportation.API/Controllers/Admin/RouteManagementController.cs
+++ TourismSmartTransportation.API/Controllers/Admin/RouteManagementController.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TourismSmartTransportation.API.Validation;
 using TourismSmartTransportation.Business.Interfaces.Partner;
+using TourismSmartTransportation.Business.SearchModel.Partner.Route;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,12 +21,19 @@ namespace TourismSmartTransportation.API.Controllers.Admin
         {
             _service = service;
         }
-    
+
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             return SendResponse(await _service.GetAll());
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(NotAllowedNullPropertiesAttribute))]
+        public async Task<IActionResult> CreateRoute([FromBody] CreateRouteModel model)
+        {
+            return SendResponse(await _service.CreateRoute(model));
         }
     }
 }
---
+++ TourismSmartTransportation.Business/SearchModel/Partner/Route/CreateStationRoute.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TourismSmartTransportation.Business.SearchModel.Partner.Route
+{
+    public class CreateStationRoute
+    {
+        public int OrderNumber { get; set; }
+        public Guid StationId { get; set; }
+        public Guid? RouteId { get; set; }
+    }
+}
