900f1300165b50d36a150bd314bfa9f9093ee610
danhnlc <danhnlc252k@gmail.com>
2022-06-28 11:42:59 +0700

    update deployment batch

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 @echo off
 dotnet build . --configuration Release
-dotnet publish . -p:PublishProfile=TourismSmartTransportation.API\Properties\PublishProfiles\FTP.pubxml
+dotnet publish . -p:PublishProfile=TourismSmartTransportation.API\Properties\PublishProfiles\tourism-smart-transportation-api-WebDeploy.pubxml
 if %errorlevel%==0 (echo Done!) else (echo Failed!)
 pause
