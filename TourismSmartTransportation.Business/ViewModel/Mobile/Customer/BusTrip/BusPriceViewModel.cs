cdf3dc3329c073d31e35f7d3743015f5780ac580
BasicZero <visualgianguyen@gmail.com>
2022-08-04 17:54:02 +0700

    get bus price done

---
+++ TourismSmartTransportation.Business/ViewModel/Mobile/Customer/BusTrip/BusPriceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourismSmartTransportation.Business.ViewModel.Mobile.Customer.BusTrip
+{
+    public class BusPriceViewModel
+    {
+        public string Name { get; set; }
+        public int TotalStation { get; set; }
+        public decimal Distance { get; set; }
+        public decimal Price { get; set; }
+    }
+}
