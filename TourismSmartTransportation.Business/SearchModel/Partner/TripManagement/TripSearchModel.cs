d9f43dc18f339330acf29f3952be0dec9e1ad0d8
BasicZero <visualgianguyen@gmail.com>
2022-08-15 10:05:23 +0700

    update trip

--- TourismSmartTransportation.Business/SearchModel/Partner/TripManagement/TripSearchModel.cs
+++ TourismSmartTransportation.Business/SearchModel/Partner/TripManagement/TripSearchModel.cs
@@ -12,5 +12,7 @@ namespace TourismSmartTransportation.Business.SearchModel.Partner.Route
         public DateTime? TimeStart { get; set; }
         public DateTime? TimeEnd { get; set; }
         public int? Status { get; set; }
+
+        public string Week { get; set; }
     }
 }
