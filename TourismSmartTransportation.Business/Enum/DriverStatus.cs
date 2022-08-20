08441918101a0aebf6bb7320c6309d417d4acb86
danhnlc <danhnlc252k@gmail.com>
2022-08-20 10:21:45 +0700

    signalr section4

--- TourismSmartTransportation.Business/Enum/DriverStatus.cs
+++ TourismSmartTransportation.Business/Enum/DriverStatus.cs
@@ -3,5 +3,6 @@ public enum DriverStatus
     Disabled = 0, // Tài xế bị vô hiệu quá
     Active = 1, // Đang hoạt động đối với mọi tài xế
     On = 2, // Mở chế độ nhận request - dịch vụ đặt xe
-    Off = 3 // Tắt chế độ nhận request - dịch vụ đặt xe
+    Off = 3, // Tắt chế độ nhận request - dịch vụ đặt xe
+    OnBusy = 4, // Tài xế đã và đang nhận yêu cầu từ một khách hàng
 }
